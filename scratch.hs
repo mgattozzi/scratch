e35f5781b2a2f213f56b3276fb40a5689d6b8d32
Michael Gattozzi <mgattozzi@gmail.com>
2015-02-07 18:04:54 -0500

    Minor changes to remove function

--- scratch.hs
+++ scratch.hs
@@ -1,11 +1,17 @@
 --Acts as a scratch pad for ideas to be referenced at any time
 --from the command line
+
+--To Do
+--Figure out why one let will not work for more than the line it is on
+--Finish remove function
+
 import System.Environment
 import System.IO
 import System.Directory
 import Data.List
 import Control.Exception
 
+--Sets where the scratch pad will be located
 scratchpad = "/home/michael/.scratchpad"
 
 --Parses input commands and runs the corresponding function
@@ -37,8 +43,7 @@ view :: FilePath -> IO ()
 view filename = do
 		contents <- readFile filename
 		let splitContents = lines contents
-		let	numberedIdeas = zipWith (\n line -> show n ++ " - " ++ line) 
-								[0..] splitContents
+		let	numberedIdeas = zipWith (\n line -> show n ++ " - " ++ line) [0..] splitContents
 		mapM_ putStrLn numberedIdeas
 
 --Add ideas to the file
@@ -49,6 +54,9 @@ add _ = putStrLn "Add takes exactly two arguments"
 --Remove ideas from the file
 remove :: [String] -> IO ()
 remove [numberString] = do
+	--Turns numberString to type Char use import Data.Char and digitToInt to
+	--turn into a number
+	let number = head(head numberString)
 	putStrLn "Temporary"
 remove _ = putStrLn "You need to provide a number to delete an idea"
 
